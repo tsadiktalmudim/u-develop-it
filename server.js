46050fdc9a35170567adb8596e633ed7154258b0
Xan <tsadiktalmudim@gmail.com>
2022-04-20 19:47:25 -0500

    updated CRUD functions

--- server.js
+++ server.js
@@ -20,9 +20,36 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
 
-db.query("SELECT * FROM candidates", (err, rows) => {
-  console.log(rows);
+// db.query("SELECT * FROM candidates", (err, rows) => {
+//   console.log(rows);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connections) 
+//               VALUES (?,?,?,?)`;
+// const params = [];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
   // Default response for any other request (Not Found)
